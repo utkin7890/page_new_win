7491813f875d3c8a5a42d8bd3f4a2fbee321daa8
utkin7890 <58480491+utkin7890@users.noreply.github.com>
2020-11-12 10:15:52 +0300

    Update forpy.py

--- forpy.py
+++ forpy.py
@@ -1,1 +1,43 @@
-sadf
+
+<!DOCTYPE html>
+
+<html>
+<head>
+<meta charset = 'utf-8'>
+<title>Страница 0</title>
+</head>
+
+<body>
+  <p>
+<a href='https://utkin7890.github.io/page_new_win/one.html'>
+  <font size="20">-----1</font>
+    </a>
+
+    </p>
+  <br/>
+  <br/>
+  <br/>
+  <p>
+<a href='https://utkin7890.github.io/page_new_win/zero.html'>
+  <font size="20">-----0</font>
+    </a>
+
+    </p>
+   <br/>
+  <br/>
+  <br/>
+  
+    <p>
+<a href='https://utkin7890.github.io/page_new_win/one.html' class="qqq111">
+  <font size="20">-----qqq111</font>
+    </a>
+     
+    </p>
+    
+ <div id="element_id">
+     <p>some content</p>
+</div>   
+    
+    
+</body>
+</html>
